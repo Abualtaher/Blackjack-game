b77d58cfb7a27f804aa224fd3ebaf798e80b4374
Abualtaher <wisamaltaher@gmail.com>
2024-09-22 20:33:39 +0200

    img

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum < 21) {
-  message = "You aer in the clear";
-} else if (sum === 21) {
-  message = "You win!!!";
-  hasBlackJack = true;
-} else {
-  message = "You lose!";
-  isAlive = false;
-}
+function startGame() {
+  if (sum < 21) {
+    message = "You aer in the clear";
+  } else if (sum === 21) {
+    message = "You win!!!";
+    hasBlackJack = true;
+  } else {
+    message = "You lose!";
+    isAlive = false;
+  }
 
-console.log(message);
+  console.log(message);
+}
