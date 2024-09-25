33b76753bb7f3f9ab5690800c877e147e7f2047e
Abualtaher <wisamaltaher@gmail.com>
2024-09-25 22:32:30 +0200

    Arrys

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let firstCard = 12;
 let secondCard = 23;
+let therdCard = 4;
+let card = [firstCard, secondCard, therdCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -9,6 +11,9 @@ let sumEl = document.querySelector("#sum-el");
 let cardEl = document.querySelector("#card-el");
 
 function startGame() {
+  renderGame();
+}
+function renderGame() {
   if (sum < 21) {
     message = "You aer in the clear";
   } else if (sum === 21) {
@@ -19,6 +24,12 @@ function startGame() {
     isAlive = false;
   }
   messageEl.textContent = message;
-  sumEl.textContent += sum;
-  cardEl.textContent = "Card : " + firstCard + " | " + secondCard;
+  sumEl.textContent = "Sum: " + sum;
+  cardEl.textContent = "Card : " + card[0] + " | " + card[1];
+}
+
+function newCard() {
+  sum += card[2];
+  renderGame();
+  cardEl.textContent += " | " + card[2];
 }
